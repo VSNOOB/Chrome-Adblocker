361e25db6226683e3cbce8e2ec90b3ddc3d6ed98
VSNOOB <83365959+VSNOOB@users.noreply.github.com>
2021-04-28 10:41:37 -0700

    Add files via upload

---
+++ background.js
@@ -0,0 +1,5 @@
+chrome.webRequest.onBeforeRequest.addListener(
+    function(details) {return{cancel: true}},
+    {urls: ["*://*.doubleclick.net/*", "*://*.facebook.net/*", "*://*.googleadservices.com/*", "*://*.googleanalytics.com/*", "ads.google.com/*", "*://*.securepubads.g.doubleclick.net/*", "*://*.ads.doubleclick.net/*"]},
+    ["blocking"] 
+)
