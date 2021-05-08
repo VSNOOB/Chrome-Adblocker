fa6947ba7e060b9737daff4e5f20836420f8cda2
VSNOOB <83365959+VSNOOB@users.noreply.github.com>
2021-05-08 08:30:39 -0700

    Update background.js

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
 chrome.webRequest.onBeforeRequest.addListener(
     function(details) {return{cancel: true}},
-    {urls: ["*://*.doubleclick.net/*", "*://*.facebook.net/*", "*://*.googleadservices.com/*", "*://*.googleanalytics.com/*", "ads.google.com/*", "*://*.securepubads.g.doubleclick.net/*", "*://*.ads.doubleclick.net/*"]},
+    {urls: ["*://*.doubleclick.net/*", "*://*.facebook.net/*", "*://*.googleadservices.com/*", "*://*.googleanalytics.com/*", "ads.google.com/*", "*://*.securepubads.g.doubleclick.net/*", "*://*.ads.doubleclick.net/*", "*://*.googleads.g.doubleclick.net/8"]},
     ["blocking"] 
 )
